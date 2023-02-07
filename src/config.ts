32bacb25f4d355ba37ffa34335c81d1569d8c56e
Trang Le <lethutrang101@gmail.com>
2023-02-07 16:04:56 +0700

    add site information

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const SITE = {
+  website: "",
+  author: "Yuko Nagakura",
+  desc: "Free patterns to knit anything your heart desires",
+  title: "Beginner's Luckraft",
+  ogImage: "v-neck-raglan-sweater.jpeg",
+  lightAndDarkMode: true
+}
